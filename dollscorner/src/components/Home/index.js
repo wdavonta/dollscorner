d45a9a79d5be3f4130bb49e3bb0e39e1a01d607c
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-05-11 21:42:19 -0400

    added slide bar, added tip sections, added to about section

--- dollscorner/src/components/Home/index.js
+++ dollscorner/src/components/Home/index.js
@@ -1,16 +1,40 @@
 import React from 'react';
+import 'react-slideshow-image/dist/styles.css';
+import { Slide } from 'react-slideshow-image';
 
+const slideImages = [
+  'images/dolls.png',
+  'images/slide_3.jpg',
+  'images/slide_4.jpg'
+];
 
 function Home() {
-  return (
-    <section className="my-5">
-      <h1 id="home">Home</h1>
-      <div className="my-2">
-        <p> Welcome
-        </p>
-      </div>
-    </section>
-  );
-}
+    return (
+        <div>
+            <h1>Welcome</h1>
+        
+
+        <div>
+          <Slide easing="ease">
+            <div className="each-slide">
+              <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+                <span>Slide 1</span>
+              </div>
+            </div>
+            <div className="each-slide">
+              <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+                <span>Slide 2</span>
+              </div>
+            </div>
+            <div className="each-slide">
+              <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+                <span>Slide 3</span>
+              </div>
+            </div>
+          </Slide>
+        </div>
+        </div>
+      )
+  };
 
 export default Home;
--- dollscorner/src/components/ParentTips/index.js
+++ dollscorner/src/components/ParentTips/index.js
@@ -1,16 +1,47 @@
 import React from 'react';
+import './style.css';
 
 
 function ParentTips() {
-  return (
-    <section className="my-5">
-      <h1 id="home">Parent Tips</h1>
-      <div className="my-2">
-        <p> Welcome
-        </p>
-      </div>
-    </section>
-  );
-}
+
+    const backendData = [
+        { title: "Tip #1", description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius", location: "Skeleton Bar" },
+        { title: "Tip #2", description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius", location: "Signature Alcoholic Drinks" },
+        { title: "Tip #3", description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius", location: "Signature Alcoholic Drinks" },
+        { title: "Tip #4", description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius", location: "Cafe La Bamba" },
+      ]
+    
+      const noteRootStyle = {
+        border: "2px #BF3F36 solid",
+        borderRadius: 9,
+        margin: 20,
+        backgroundColor: "#fffff",
+        padding: 6,
+        
+        
+
+      };
+    
+      return (
+        <div className="Inputinfo" style={{ width: 800 }}>
+          <h1 className='FC'>Parent Tips</h1>
+          {backendData.map(ele => 
+            <div style={noteRootStyle}>
+              <p>{ele.img}</p>
+              <h3 className='tline'>{ele.title}</h3>
+              <p>{ele.description}</p>
+              <div className="btns">
+              <small>Location:{ele.location}</small>
+              <button className="bb" type="button" class="btn btn" ><i class="fas fa-skull-crossbones" aria-hidden="true"></i></button>
+                </div>
+            </div>
+          )}
+          <div><p className='Content'>*Check out new food and drink recipes weekly from HHN</p></div>
+
+        </div>
+      )
+    }
+
+
 
 export default ParentTips;
