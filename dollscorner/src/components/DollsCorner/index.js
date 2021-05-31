a12becd0ac46025153c7f217a5c547192a27be32
DaVonta Williamson <davontawilliamson@DaVontas-MacBook-Pro.local>
2021-05-30 21:34:08 -0400

    added temporariy photos to show example to dolls corner page

--- dollscorner/src/components/DollsCorner/index.js
+++ dollscorner/src/components/DollsCorner/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import mangoinferno from './images/mangoinferno.jpg';
+import './style.css'
 
 function DollsCorner() {
   return (
@@ -8,15 +9,15 @@ function DollsCorner() {
       <div class="row">
         <div class="col-sm-4">
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-         
+          <img alt="" className='mi'  src={mangoinferno}/>
         </div>
         <div class="col-sm-4"> 
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-         
+          <img alt="" className='mi'  src={mangoinferno}/>
         </div>
         <div class="col-sm-4"> 
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          
+          <img alt="" className='mi'  src={mangoinferno}/>
         </div>
       </div>
     </div>
