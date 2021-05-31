abe9e876f779bb4b356902a8946b1f6806c710d4
DaVonta Williamson <davontawilliamson@DaVontas-MacBook-Pro.local>
2021-05-30 20:55:21 -0400

    updated tips section

--- dollscorner/src/components/ParentTips/index.js
+++ dollscorner/src/components/ParentTips/index.js
@@ -8,17 +8,44 @@ function ParentTips() {
       return (
 
         
-<div class="row">
-  <div class="col">
+<div class="container">
+  <div class="row align-items-start">
+    <div class="col">
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae alias corrupti odio ullam officia similique voluptate molestias perferendis quasi, qui architecto ducimus sit, est ipsum maxime at exercitationem. Quae.
+    </div>
+    <div class="col">
+      {/* One of three columns */}
+    </div>
+    <div class="col">
+      {/* One of three columns */}
+    </div>
   </div>
-  <div class="col">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis amet nulla fugiat veritatis tempora perferendis libero doloribus voluptas consequuntur! Cumque ullam omnis maxime voluptate beatae iste quod ipsum voluptatem esse excepturi culpa natus ipsam harum minima in hic nesciunt asperiores sequi perferendis ipsa modi, accusantium rerum explicabo. Dignissimos, iure, blanditiis!
+  <div class="row align-items-center">
+    <div class="col">
+      {/* One of three columns */}
+    </div>
+    <div class="col">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae alias corrupti odio ullam officia similique voluptate molestias perferendis quasi, qui architecto ducimus sit, est ipsum maxime at exercitationem. Quae.
+    </div>
+    <div class="col">
+      {/* One of three columns */}
+    </div>
   </div>
-  <div class="col">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus similique vero distinctio perspiciatis, porro assumenda illum, beatae cupiditate repellat doloribus, sed explicabo! Laudantium illum quam quasi, consequuntur ad nihil quaerat?
+  <div class="row align-items-end">
+    <div class="col">
+      {/* One of three columns */}
+    </div>
+    <div class="col">
+      {/* One of three columns */}
+    </div>
+    <div class="col">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae alias corrupti odio ullam officia similique voluptate molestias perferendis quasi, qui architecto ducimus sit, est ipsum maxime at exercitationem. Quae.
+    </div>
   </div>
 </div>
+
+
+
       )
     }
 
