5fd36a9566d7348945fa75c9778f6d73861569b8
DaVonta Williamson <wdavonta@gmail.com>
2021-07-18 22:27:15 -0400

    center contact box

--- dollscorner/src/components/Contact/index.js
+++ dollscorner/src/components/Contact/index.js
@@ -1,45 +1,53 @@
-import React, { useState } from "react";
-import './index.css';
 
-const ContactForm = () => {
-  const [status, setStatus] = useState("Submit");
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setStatus("Sending...");
-    const { name, email, message } = e.target.elements;
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-    setStatus("Submit");
-    let result = await response.json();
-    alert(result.status);
-  };
-  return (
-    <form onSubmit={handleSubmit} className='Contact'>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" required />
+import React from 'react';
+import './index.css'
+
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    }
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" className="form-control" value={this.state.name} onChange={this.onNameChange.bind(this)} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message</label>
+            <textarea className="form-control" rows="5" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
       </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" required />
-      </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" required />
-      </div>
-      <button type="submit">{status}</button>
-    </form>
-  );
-};
+    );
+  }
+
+  onNameChange(event) {
+    this.setState({name: event.target.value})
+  }
+
+  onEmailChange(event) {
+    this.setState({email: event.target.value})
+  }
+
+  onMessageChange(event) {
+    this.setState({message: event.target.value})
+  }
+
+  handleSubmit(event) {
+  }
+}
 
 export default ContactForm;
--- dollscorner/src/components/Home/index.js
+++ dollscorner/src/components/Home/index.js
@@ -1,40 +1,35 @@
 import React from 'react';
-import 'react-slideshow-image/dist/styles.css';
+import './style.css'
 import { Slide } from 'react-slideshow-image';
 
 const slideImages = [
   'images/dolls.png',
-  'images/slide_3.jpg',
+  'images/ccorn.jpeg',
   'images/slide_4.jpg'
 ];
 
-function Home() {
-    return (
-        <div>
-            <h1>Welcome</h1>
-        
-
-        <div>
-          <Slide easing="ease">
-            <div className="each-slide">
-              <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
-                <span>Slide 1</span>
-              </div>
-            </div>
-            <div className="each-slide">
-              <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
-                <span>Slide 2</span>
-              </div>
-            </div>
-            <div className="each-slide">
-              <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-                <span>Slide 3</span>
-              </div>
-            </div>
-          </Slide>
+const Home = () => {
+  return (
+    <div>
+      <Slide easing="ease">
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+            <span>Slide 1</span>
+          </div>
         </div>
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+            <span>Slide 2</span>
+          </div>
         </div>
-      )
-  };
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+            <span>Slide 3</span>
+          </div>
+        </div>
+      </Slide>
+    </div>
+  )
+};
 
 export default Home;
