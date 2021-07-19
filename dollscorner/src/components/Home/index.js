89703ce1d0db4fc8180cc169f63b5c92b65dce4f
DaVonta Williamson <wdavonta@gmail.com>
2021-07-18 23:55:23 -0400

    add carousel to page, adding pictures later

--- dollscorner/src/components/Home/index.js
+++ dollscorner/src/components/Home/index.js
@@ -1,35 +1,74 @@
-import React from 'react';
-import './style.css'
-import { Slide } from 'react-slideshow-image';
+// import React from 'react';
+// import './style.css'
+// import { Slide } from 'react-slideshow-image';
 
-const slideImages = [
-  'images/dolls.png',
-  'images/ccorn.jpeg',
-  'images/slide_4.jpg'
+// const slideImages = [
+//   'images/dolls.png',
+//   'images/ccorn.jpeg',
+//   'images/slide_4.jpg'
+// ];
+
+// const Home = () => {
+//   return (
+//     <div>
+//       <Slide easing="ease">
+//         <div className="each-slide">
+//           <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+//             <span>Slide 1</span>
+//           </div>
+//         </div>
+//         <div className="each-slide">
+//           <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+//             <span>Slide 2</span>
+//           </div>
+//         </div>
+//         <div className="each-slide">
+//           <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+//             <span>Slide 3</span>
+//           </div>
+//         </div>
+//       </Slide>
+//     </div>
+//   )
+// };
+
+// export default Home;
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Carousel from "react-elastic-carousel";
+import Item from "./Item";
+import "./style.css";
+
+
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 4 },
 ];
 
-const Home = () => {
+function Home() {
   return (
-    <div>
-      <Slide easing="ease">
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
-            <span>Slide 1</span>
-          </div>
-        </div>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
-            <span>Slide 2</span>
-          </div>
-        </div>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-            <span>Slide 3</span>
-          </div>
-        </div>
-      </Slide>
-    </div>
-  )
-};
+    <>
+      <h1 style={{ textAlign: "center" }}>Example to setup your carousel in react</h1>
+      <div className="App">
+        <Carousel breakPoints={breakPoints}>
+          <Item>One</Item>
+          <Item>Two</Item>
+          <Item>Three</Item>
+          <Item>Four</Item>
+          <Item>Five</Item>
+          <Item>Six</Item>
+          <Item>Seven</Item>
+          <Item>Eight</Item>
+        </Carousel>
+      </div>
+    </>
+  );
+}
 
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Home />, rootElement);
 export default Home;
--- dollscorner/src/components/Nav/NavbarElements.js
+++ dollscorner/src/components/Nav/NavbarElements.js
@@ -3,7 +3,7 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-  background: #b76c77;
+  background: #000000;
   height: 80px;
   display: flex;
   justify-content: space-between;
